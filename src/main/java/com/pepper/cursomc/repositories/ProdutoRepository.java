a03382649ec9c109f8d97b810c71c61fb9ab258a
RodrigoPimenta <rodrigospimenta@gmail.com>
2020-04-01 22:47:25 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/pepper/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.pepper.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pepper.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+}
