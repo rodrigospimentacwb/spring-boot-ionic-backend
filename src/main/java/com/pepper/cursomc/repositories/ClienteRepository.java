527c7da0d155776ee24cecde12fd3b087e88206c
RodrigoPimenta <rodrigospimenta@gmail.com>
2020-06-14 14:01:00 -0300

    Cliente,TipoCliente, telefone e enderecos

---
+++ src/main/java/com/pepper/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.pepper.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pepper.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+}
---
+++ src/main/java/com/pepper/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.pepper.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pepper.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+}
