e8042fdd92d380b3e2952df571c2b6446848178d
RodrigoPimenta <rodrigospimenta@gmail.com>
2020-06-13 21:16:52 -0300

    Estado e Cidade

---
+++ src/main/java/com/pepper/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.pepper.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pepper.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+}
