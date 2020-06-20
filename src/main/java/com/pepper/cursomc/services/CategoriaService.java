74c0aa178dc77bf42aeeb3acdd6bbe07f093b485
RodrigoPimenta <rodrigospimenta@gmail.com>
2020-06-20 09:54:09 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/pepper/cursomc/services/CategoriaService.java
+++ src/main/java/com/pepper/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pepper.cursomc.domain.Categoria;
+import com.pepper.cursomc.dto.CategoriaDTO;
 import com.pepper.cursomc.repositories.CategoriaRepository;
 import com.pepper.cursomc.services.exceptions.DataIntegrityException;
 import com.pepper.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageResquest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageResquest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
