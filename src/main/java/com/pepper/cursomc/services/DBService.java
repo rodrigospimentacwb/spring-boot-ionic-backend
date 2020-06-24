e0185e171653e0323168723f46ac02790292e23f
RodrigoPimenta <rodrigospimenta@gmail.com>
2020-06-24 19:07:26 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/pepper/cursomc/services/DBService.java
+++ src/main/java/com/pepper/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.pepper.cursomc.domain.PagamentoComCartao;
 import com.pepper.cursomc.domain.Pedido;
 import com.pepper.cursomc.domain.Produto;
 import com.pepper.cursomc.domain.enums.EstadoPagamento;
+import com.pepper.cursomc.domain.enums.Perfil;
 import com.pepper.cursomc.domain.enums.TipoCliente;
 import com.pepper.cursomc.repositories.CategoriaRepository;
 import com.pepper.cursomc.repositories.CidadeRepository;
@@ -125,12 +126,19 @@ public class DBService {
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "rodrigospimenta@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "safeface.app@gmail.com", "87213522086", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("35248954","991848587"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Av Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Av Floriano", "2106", null, "Centro", "80254852", cli2, c2);
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 				
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
